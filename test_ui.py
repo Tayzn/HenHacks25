667a9a7c4c4a0b248d3683696e8030e135eaeb8b
Olivia Bouvier <obouvier@udel.edu>
2025-03-01 16:50:07 -0500

    Added pomodoro timer

--- test_ui.py
+++ test_ui.py
@@ -18,13 +18,13 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QGroupBox, QHBoxLayout, QLabel,
     QLineEdit, QListWidget, QListWidgetItem, QMainWindow,
     QMenuBar, QPushButton, QSizePolicy, QSlider,
-    QStatusBar, QTextEdit, QVBoxLayout, QWidget)
+    QStatusBar, QTimeEdit, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1119, 858)
+        MainWindow.resize(1119, 710)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayout_10 = QHBoxLayout(self.centralwidget)
@@ -49,30 +49,41 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_5.addWidget(self.timeLabel)
 
-        self.timeLabelx = QTextEdit(self.groupBox_2)
-        self.timeLabelx.setObjectName(u"timeLabelx")
-
-        self.verticalLayout_5.addWidget(self.timeLabelx)
-
         self.label = QLabel(self.groupBox_2)
         self.label.setObjectName(u"label")
 
         self.verticalLayout_5.addWidget(self.label)
 
-        self.appUsage = QTextEdit(self.groupBox_2)
-        self.appUsage.setObjectName(u"appUsage")
+        self.startPomodoroButton = QPushButton(self.groupBox_2)
+        self.startPomodoroButton.setObjectName(u"startPomodoroButton")
 
-        self.verticalLayout_5.addWidget(self.appUsage)
+        self.verticalLayout_5.addWidget(self.startPomodoroButton)
+
+        self.pausePomodoroButton = QPushButton(self.groupBox_2)
+        self.pausePomodoroButton.setObjectName(u"pausePomodoroButton")
+
+        self.verticalLayout_5.addWidget(self.pausePomodoroButton)
+
+        self.resetPomodoroButton = QPushButton(self.groupBox_2)
+        self.resetPomodoroButton.setObjectName(u"resetPomodoroButton")
+
+        self.verticalLayout_5.addWidget(self.resetPomodoroButton)
+
+        self.label_5 = QLabel(self.groupBox_2)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+
+        self.verticalLayout_5.addWidget(self.label_5)
+
+        self.pomodoroTimer = QLineEdit(self.groupBox_2)
+        self.pomodoroTimer.setObjectName(u"pomodoroTimer")
+
+        self.verticalLayout_5.addWidget(self.pomodoroTimer)
 
         self.label_3 = QLabel(self.groupBox_2)
         self.label_3.setObjectName(u"label_3")
 
         self.verticalLayout_5.addWidget(self.label_3)
-
-        self.music = QTextEdit(self.groupBox_2)
-        self.music.setObjectName(u"music")
-
-        self.verticalLayout_5.addWidget(self.music)
 
         self.volumeControl = QSlider(self.groupBox_2)
         self.volumeControl.setObjectName(u"volumeControl")
@@ -97,6 +108,31 @@ class Ui_MainWindow(object):
         self.focusMode.setObjectName(u"focusMode")
 
         self.verticalLayout_4.addWidget(self.focusMode)
+
+        self.label_2 = QLabel(self.groupBox)
+        self.label_2.setObjectName(u"label_2")
+
+        self.verticalLayout_4.addWidget(self.label_2)
+
+        self.reminderList = QListWidget(self.groupBox)
+        self.reminderList.setObjectName(u"reminderList")
+
+        self.verticalLayout_4.addWidget(self.reminderList)
+
+        self.reminderInput = QLineEdit(self.groupBox)
+        self.reminderInput.setObjectName(u"reminderInput")
+
+        self.verticalLayout_4.addWidget(self.reminderInput)
+
+        self.reminderTimeEdit = QTimeEdit(self.groupBox)
+        self.reminderTimeEdit.setObjectName(u"reminderTimeEdit")
+
+        self.verticalLayout_4.addWidget(self.reminderTimeEdit)
+
+        self.addReminderButton = QPushButton(self.groupBox)
+        self.addReminderButton.setObjectName(u"addReminderButton")
+
+        self.verticalLayout_4.addWidget(self.addReminderButton)
 
         self.label_4 = QLabel(self.groupBox)
         self.label_4.setObjectName(u"label_4")
@@ -150,22 +186,19 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.groupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"GroupBox", None))
+        self.groupBox_2.setTitle("")
         self.timeLabel.setText(QCoreApplication.translate("MainWindow", u"Timer (displays current time)", None))
-        self.timeLabelx.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"App Usage", None))
+        self.startPomodoroButton.setText(QCoreApplication.translate("MainWindow", u"Start Pomodoro", None))
+        self.pausePomodoroButton.setText(QCoreApplication.translate("MainWindow", u"Pause Pomodoro", None))
+        self.resetPomodoroButton.setText(QCoreApplication.translate("MainWindow", u"Reset Pomodoro", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Pomodoro Timer", None))
+        self.pomodoroTimer.setText("")
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Music Controls", None))
-        self.music.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
-        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"GroupBox", None))
+        self.groupBox.setTitle("")
         self.focusMode.setText(QCoreApplication.translate("MainWindow", u"Focus Mode", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Reminders", None))
+        self.addReminderButton.setText(QCoreApplication.translate("MainWindow", u"Add Reminder", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"To-Do List", None))
         self.addTaskButton.setText(QCoreApplication.translate("MainWindow", u"Add Task", None))
         self.clearCheckedButton.setText(QCoreApplication.translate("MainWindow", u"Clear Checked Items", None))
--- window.py
+++ window.py
@@ -1,143 +1,231 @@
+import os
+import sys
+from apscheduler.schedulers.background import BackgroundScheduler
 from PyQt6 import uic
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QPushButton, QLabel, QListWidget, QLineEdit, 
-    QListWidgetItem, QSystemTrayIcon, QMenu
+    QListWidgetItem, QSystemTrayIcon, QMenu, QTimeEdit, QMessageBox
 )
-from PyQt6.QtGui import QGuiApplication
-from PyQt6.QtGui import QAction
-from PyQt6.QtCore import QTimer, QTime, Qt
-from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import QPropertyAnimation, QEasingCurve
-import sys
+from PyQt6.QtGui import QGuiApplication, QAction, QIcon
+from PyQt6.QtCore import QTimer, QTime, Qt, QPropertyAnimation, QEasingCurve
+from datetime import datetime, timedelta
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("test.ui", self)
 
-        # Resize window
+        # Resize and position window
         self.resize(800, 600)
-        self.move_to_bottom_right()  # Move window to bottom-right
+        self.move_to_bottom_right()
+        self.load_stylesheet("style.qss")
 
-        # Find UI elements
+        # UI Elements
         self.time_label = self.findChild(QLabel, "timeLabel")
-        
         self.task_input = self.findChild(QLineEdit, "taskInput")
         self.add_task_button = self.findChild(QPushButton, "addTaskButton")
         self.task_list = self.findChild(QListWidget, "taskList")
         self.clear_checked_button = self.findChild(QPushButton, "clearCheckedButton")
         
+        self.reminder_input = self.findChild(QLineEdit, "reminderInput")
+        self.reminder_list = self.findChild(QListWidget, "reminderList")
+        self.add_reminder_button = self.findChild(QPushButton, "addReminderButton")
+        self.reminder_time_edit = self.findChild(QTimeEdit, "reminderTimeEdit")
+        self.reminder_time_edit.setDisplayFormat("HH:mm")
+        
+        # Pomodoro Timer UI
+        self.pomodoro_timer = self.findChild(QLineEdit, "pomodoroTimer")
+        self.start_pomodoro_button = self.findChild(QPushButton, "startPomodoroButton")
+        self.pause_pomodoro_button = self.findChild(QPushButton, "pausePomodoroButton")
+        self.reset_pomodoro_button = self.findChild(QPushButton, "resetPomodoroButton")
+        
+        # Connect Buttons
+        self.add_task_button.clicked.connect(self.add_task)
+        self.clear_checked_button.clicked.connect(self.clear_checked_tasks)
+        self.add_reminder_button.clicked.connect(self.add_reminder)
+        self.start_pomodoro_button.clicked.connect(self.start_pomodoro)
+        self.pause_pomodoro_button.clicked.connect(self.pause_pomodoro)
+        self.reset_pomodoro_button.clicked.connect(self.reset_pomodoro)
+        
+        self.time_for_break= True;
+        
+
+        # Timers
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+        
+        self.reminder_timer = QTimer(self)
+        self.reminder_timer.timeout.connect(self.check_reminders)
+        self.reminder_timer.start(60000)
+
+        # System Tray Setup
+        self.tray_icon = QSystemTrayIcon(QIcon("Tomato-Whole-Red-1-Clip-Art.png"), self)
+        self.tray_icon.setToolTip("PomKit")
+        tray_menu = QMenu()
+        tray_menu.addAction(QAction("Show", self, triggered=self.show_window))
+        tray_menu.addAction(QAction("Quit", self, triggered=self.close_app))
+        self.tray_icon.setContextMenu(tray_menu)
+        self.tray_icon.activated.connect(self.tray_icon_clicked)
+        self.tray_icon.show()
+        
+        # Pomodoro Variables
+        self.pomodoro_time = 10 #DON"T FORGET TO CHANGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        self.break_time = 5 * 60
+        self.current_time = self.pomodoro_time
+        self.pomodoro_running = False
+        
+        # Animation
         self.animation = QPropertyAnimation(self, b"geometry")
         self.animation.setDuration(500)
         self.animation.setEasingCurve(QEasingCurve.Type.InBounce)
         
-        if self.task_list is None:
-            print("Error: taskList not found in UI!")
-        if self.time_label is None:
-            print("Error: timeLabel not found in UI!")
-        if self.add_task_button is None:
-            print("Error: addTaskButton not found in UI!")
-        if self.task_list is None:
-            print("Error: taskList not found in UI!")
-        if self.clear_checked_button is None:
-            print("Error: clearCheckedButton not found in UI!")
+        # Scheduler for Pomodoro
+        self.scheduler = BackgroundScheduler()
+        self.scheduler.start()
+        
+        self.reminders = []
+        self.update_time()
 
-        # Connect buttons
-        self.add_task_button.clicked.connect(self.add_task)
-        self.clear_checked_button.clicked.connect(self.clear_checked_tasks)
+    def start_pomodoro(self):
+        """Start the Pomodoro Timer and ensure display updates every second."""
+        if not self.pomodoro_running:
+            self.pomodoro_running = True
+            self.current_time = self.pomodoro_time
 
-        # Set up a timer to update time
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_time)
-        self.timer.start(1000)  # Update every second
-        self.update_time()  # Initialize the time display
+            # Disconnect any previous connections to avoid multiple triggers
+            try:
+                self.timer.timeout.disconnect(self.update_pomodoro_display)
+            except TypeError:
+                pass  # Ignore if there was nothing to disconnect
 
-        # Set up system tray icon
-        self.tray_icon = QSystemTrayIcon(self)
-        self.tray_icon.setIcon(QIcon("Tomato-Whole-Red-1-Clip-Art.png"))  # Set an icon (replace with a real file)
-        self.tray_icon.setToolTip("PomKit")
+            self.timer.timeout.connect(self.update_pomodoro_display)
+            self.timer.start(1000)  # Update every second
+            self.update_pomodoro_display()  # Ensure UI updates immediately
+    
+    def pause_pomodoro(self):
+        self.pomodoro_running = False
+        self.timer.stop()
 
-        # Create tray menu
-        self.tray_menu = QMenu()
+    def reset_pomodoro(self):
+        """Reset the Pomodoro Timer."""
+        self.pomodoro_running = False
+        self.timer.stop()
+        self.current_time = self.pomodoro_time
+        self.update_pomodoro_display()
 
-        show_action = QAction("Show", self)
-        show_action.triggered.connect(self.show_window)
-        self.tray_menu.addAction(show_action)
+    def update_pomodoro_display(self):
+        """Update the Pomodoro Timer Display every second."""
+        if self.pomodoro_running and self.current_time > 0:
+            minutes, seconds = divmod(self.current_time, 60)
+            self.pomodoro_timer.setText(f"{minutes:02}:{seconds:02}")
+            self.current_time -= 1  # Decrease time by 1 second
+        else:
+            self.timer.stop()  # Stop the timer when countdown reaches zero
+            if self.time_for_break:
+                self.show_pomodoro_notification_break()
+            else:
+                self.show_pomodoro_notification_work()
 
-        quit_action = QAction("Quit", self)
-        quit_action.triggered.connect(self.close_app)
-        self.tray_menu.addAction(quit_action)
+    def schedule_pomodoro_end(self, duration):
+        """Schedule an event when the Pomodoro session ends."""
+        current_datetime = datetime.now()  # Get current date & time
+        end_datetime = current_datetime + timedelta(seconds=duration)  # Add duration
+        self.scheduler.add_job(self.show_pomodoro_notification_break, "date", run_date=end_datetime)
 
-        self.tray_icon.setContextMenu(self.tray_menu)
-        self.tray_icon.activated.connect(self.tray_icon_clicked)
+    def show_pomodoro_notification_break(self):
+        """Notify user about break time and wait before restarting."""
+        QMessageBox.information(self, "Pomodoro Completed!", "⏳ Time's up! Take a break!")
+        self.current_time = self.break_time
+        self.pomodoro_running = False
+        self.time_for_break = False  # Next session will be work
 
-        self.tray_icon.show()  # Show tray icon
+        # Wait before starting the break countdown
+        QTimer.singleShot(1000, self.start_pomodoro)
+        
+    def show_pomodoro_notification_work(self):
+        """Notify user to get back to work and wait before restarting."""
+        QMessageBox.information(self, "Break Over!", "⏳ Time to get back to work!")
+        self.current_time = self.pomodoro_time
+        self.pomodoro_running = False
+        self.time_for_break = True  # Next session will be break
+
+        # Wait before starting the work countdown
+        QTimer.singleShot(1000, self.start_pomodoro)
 
     def update_time(self):
-        """Update the clock display."""
-        current_time = QTime.currentTime().toString("HH:mm:ss")
-        self.time_label.setText(current_time)
+        self.time_label.setText("Current time: " + QTime.currentTime().toString("HH:mm:ss"))
 
     def add_task(self):
-        """Add a new task to the to-do list with a checkbox."""
         task = self.task_input.text().strip()
         if task:
             item = QListWidgetItem(task)
-            item.setFlags(item.flags() | Qt.ItemFlag.ItemIsUserCheckable)
+            item.setFlags(item.flags() | Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled)
             item.setCheckState(Qt.CheckState.Unchecked)
             self.task_list.addItem(item)
             self.task_input.clear()
 
+    def add_reminder(self):
+        reminder_text = self.reminder_input.text().strip()
+        reminder_time = self.reminder_time_edit.time().toString("HH:mm")
+        if reminder_text:
+            self.reminders.append((reminder_time, reminder_text))
+            self.reminder_list.addItem(f"{reminder_time} - {reminder_text}")
+            self.reminder_input.clear()
+
+    def check_reminders(self):
+        current_time = QTime.currentTime().toString("HH:mm")
+        for reminder_time, reminder_text in self.reminders:
+            if reminder_time == current_time:
+                self.show_reminder_dialog(reminder_text)
+                self.reminders.remove((reminder_time, reminder_text))
+
+    def show_reminder_dialog(self, message):
+        QMessageBox.information(self, "Reminder Alert", f"Reminder: {message}")
+
     def clear_checked_tasks(self):
-        """Remove checked tasks from the list."""
         for i in range(self.task_list.count() - 1, -1, -1):
             item = self.task_list.item(i)
             if item.checkState() == Qt.CheckState.Checked:
                 self.task_list.takeItem(i)
 
     def closeEvent(self, event):
-        """Override the close event to hide the window instead of quitting."""
         event.ignore()
         self.hide()
         self.tray_icon.showMessage("To-Do List", "App minimized to tray", QSystemTrayIcon.MessageIcon.Information, 2000)
 
     def show_window(self):
-        """Restore the window from the tray."""
         self.showNormal()
         self.activateWindow()
 
     def close_app(self):
-        """Quit the application."""
         self.tray_icon.hide()
         QApplication.quit()
 
     def tray_icon_clicked(self, reason):
-        """Show window when the tray icon is clicked."""
         if reason == QSystemTrayIcon.ActivationReason.Trigger:
             self.show_window()
             
-    def showEvent(self, event):
-        """Animate the window when it is shown."""
-        start_rect = self.geometry().adjusted(0, -50, 0, -50)
-        self.animation.setStartValue(start_rect)
-        self.animation.setEndValue(self.geometry())
-        self.animation.start()
-        super().showEvent(event)  # Ensure normal show behavior
-        
     def move_to_bottom_right(self):
         """Move the window to the bottom-right corner of the screen."""
-        screen = QGuiApplication.primaryScreen().availableGeometry()  # Get screen size
+        screen = QGuiApplication.primaryScreen().availableGeometry()  # Get available screen size
         window_size = self.frameGeometry()  # Get window size
 
-        x = screen.width() - window_size.width()  # Calculate X position
-        y = screen.height() - window_size.height()  # Calculate Y position
+        x = screen.right() - window_size.width()  # Adjust X position
+        y = screen.bottom() - window_size.height()  # Adjust Y position
 
         self.move(x, y)  # Move window
         
-        
-app = QApplication(sys.argv)
-app.setQuitOnLastWindowClosed(False)  # Prevent app from quitting when window is closed
+    def load_stylesheet(self, filename):
+        """Load an external QSS stylesheet and apply it."""
+        if os.path.exists(filename):
+            with open(filename, "r") as file:
+                self.setStyleSheet(file.read())
+        else:
+            print(f"Error: Stylesheet '{filename}' not found!")
 
+app = QApplication(sys.argv)
+app.setQuitOnLastWindowClosed(False)
 window = MyWindow()
 window.show()
 app.exec()
