4e28a95981288ae953e5fa2844a7464259744415
Tayzn <tyguy2464@gmail.com>
2025-03-01 14:15:58 -0500

    polling active window

--- modules/polling.py
+++ modules/polling.py
@@ -1,8 +1,23 @@
 import time
+import pywinctl as pywin
 from PyQt6.QtCore import QThread
 
+import psutil
+def get_name_from_pid(pid):
+    return psutil.Process(pid).name()
+
 class PollingThread(QThread):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.storedActiveWindowName = ""
+
     def run(self):
         while True:
-            print('polling!')
-            time.sleep(1)
+            currentWindow = pywin.getActiveWindow()
+            currentAppName = get_name_from_pid(currentWindow.getPID())
+
+            if currentAppName != self.storedActiveWindowName:
+                self.storedActiveWindowName = currentAppName
+                print("Changed!", currentWindow.title)
+                
+            time.sleep(0.3)
