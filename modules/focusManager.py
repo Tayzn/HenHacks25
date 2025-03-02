d0bb36c323e6242aeff9b060d0278b8dc49fe338
Tayzn <tyguy2464@gmail.com>
2025-03-02 08:41:20 -0500

    Fix focus mode button change initial

--- modules/focusManager.py
+++ modules/focusManager.py
@@ -6,6 +6,8 @@ class FocusSession():
     self.appTimeline = []
     self.app = app
 
+    self.app.get_window("MainWindow").focusButton.setText("In Focus Mode: " + str(self.duration) + " seconds")
+
     self.timer = QTimer()
     self.timer.timeout.connect(self.tick_timer)
     self.timer.start(1000)
@@ -13,8 +15,6 @@ class FocusSession():
   def tick_timer(self):
     self.duration += 1
     self.app.get_window("MainWindow").focusButton.setText("In Focus Mode: " + str(self.duration) + " seconds")
-    if self.duration % 3 == 0:
-      print(self.appTimeline)
 
   def ping_app_timeline(self, appName, whitelisted):
     self.appTimeline.append({"appName": appName, "time": self.duration, "whitelisted": whitelisted})
@@ -22,12 +22,6 @@ class FocusSession():
   def close(self):
     self.timer.stop()
     self.app.get_window("MainWindow").focusButton.setText("Start Focus Mode")
-
-'''
-  time spent on each non-focused app
-  how many times you switched into a non-focused app (more than 1 minute)
-  timeline of interruptions (>1 minute)
- ''' 
 
 class FocusManager():
   def __init__(self, app):
@@ -37,7 +31,6 @@ class FocusManager():
 
   def toggle_focus_mode(self):
     self.focusMode = not self.focusMode
-    print('focus is:', self.focusMode)
     if self.focusMode:
       self.currentSession = FocusSession(self.app)
     elif self.currentSession:
